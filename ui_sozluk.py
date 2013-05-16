125a9bb3d9b6f381efd414e8e94a5b6a1a0eb532
Uğurcan Ergün <ugurcanergn@gmail.com>
2013-05-16 13:55:42 +0300

    UI is redesigned in order to use seslisozluk API

--- ui_sozluk.py
+++ ui_sozluk.py
@@ -2,8 +2,8 @@
 
 # Form implementation generated from reading ui file 'sozluk.ui'
 #
-# Created: Mon Apr 30 01:12:20 2012
-#      by: PyQt4 UI code generator 4.8.5
+# Created: Thu May 16 13:34:59 2013
+#      by: PyQt4 UI code generator 4.9.1
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -17,8 +17,7 @@ except AttributeError:
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName(_fromUtf8("MainWindow"))
-        MainWindow.resize(459, 394)
-        MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "Clipdict", None, QtGui.QApplication.UnicodeUTF8))
+        MainWindow.resize(459, 380)
         self.centralwidget = QtGui.QWidget(MainWindow)
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
         self.verticalLayout = QtGui.QVBoxLayout(self.centralwidget)
@@ -29,27 +28,39 @@ class Ui_MainWindow(object):
         self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
         self.horizontalLayout.addWidget(self.lineEdit)
         self.pushButton = QtGui.QPushButton(self.centralwidget)
-        self.pushButton.setText(QtGui.QApplication.translate("MainWindow", "Ara", None, QtGui.QApplication.UnicodeUTF8))
         self.pushButton.setObjectName(_fromUtf8("pushButton"))
         self.horizontalLayout.addWidget(self.pushButton)
+        self.pushButton_2 = QtGui.QPushButton(self.centralwidget)
+        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
+        self.horizontalLayout.addWidget(self.pushButton_2)
         self.verticalLayout.addLayout(self.horizontalLayout)
-        self.webView = QtWebKit.QWebView(self.centralwidget)
-        self.webView.setUrl(QtCore.QUrl(_fromUtf8("about:blank")))
-        self.webView.setObjectName(_fromUtf8("webView"))
-        self.verticalLayout.addWidget(self.webView)
+        self.listWidget = QtGui.QListWidget(self.centralwidget)
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        font.setBold(True)
+        font.setItalic(True)
+        font.setWeight(75)
+        self.listWidget.setFont(font)
+        self.listWidget.setSelectionMode(QtGui.QAbstractItemView.NoSelection)
+        self.listWidget.setFlow(QtGui.QListView.LeftToRight)
+        self.listWidget.setProperty("isWrapping", True)
+        self.listWidget.setObjectName(_fromUtf8("listWidget"))
+        self.verticalLayout.addWidget(self.listWidget)
+        self.horizontalLayout_2 = QtGui.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(_fromUtf8("horizontalLayout_2"))
+        self.comboBox = QtGui.QComboBox(self.centralwidget)
+        self.comboBox.setObjectName(_fromUtf8("comboBox"))
+        self.horizontalLayout_2.addWidget(self.comboBox)
+        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.horizontalLayout_2.addItem(spacerItem)
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
         MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtGui.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 459, 25))
-        self.menubar.setObjectName(_fromUtf8("menubar"))
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtGui.QStatusBar(MainWindow)
-        self.statusbar.setObjectName(_fromUtf8("statusbar"))
-        MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
-        pass
+        MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "Clipdict", None, QtGui.QApplication.UnicodeUTF8))
+        self.pushButton.setText(QtGui.QApplication.translate("MainWindow", "Ara", None, QtGui.QApplication.UnicodeUTF8))
+        self.pushButton_2.setText(QtGui.QApplication.translate("MainWindow", "Panodan Al", None, QtGui.QApplication.UnicodeUTF8))
 
-from PyQt4 import QtWebKit
